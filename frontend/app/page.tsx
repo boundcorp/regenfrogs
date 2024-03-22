26cf499ae2b597c932cd6f9bec1a78b3a73ff1c9
Audrey Bound <audrey@boundcorp.net>
2024-03-22 12:56:18 -0700

    wip: homepage, backend frog profile lists

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,15 @@
 import { getFrameMetadata } from 'frog/next'
 import type { Metadata } from 'next'
-import Image from 'next/image'
+import Typography from "@mui/material/Typography"
+import Card from "@mui/material/Card"
+import localFont from "next/font/local"
 
 import styles from './page.module.css'
+import Link from 'next/link'
 
+const myFont = localFont({ src: './fonts/Catboo.ttf' })
+
+/*
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
     `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
@@ -12,108 +18,20 @@ export async function generateMetadata(): Promise<Metadata> {
     other: frameTags,
   }
 }
+*/
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>app/page.tsx</code>
-          </p>
-          <p>
-            Head to{' '}
-            <a
-              href="/api/dev"
-              style={{ display: 'inline', fontWeight: 'semibold' }}
-            >
-              <code className={styles.code}>localhost:3000/api</code>
-            </a>{' '}
-            for your frame endpoint.
-          </p>
-        </div>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={myFont.className} style={{ fontSize: '10em' }}>REGENFROGS</div>
+      <Card sx={{ padding: '1em', fontFamily: "Helvetiva Now Display" }}>
+        <Typography variant="h3">Adopt Your Frog</Typography><Typography variant="h5" mb={"1em"}>Choose your virtual pet frog and begin your digital journey.</Typography>
+        <Typography variant="h3">Care for Your Frog</Typography><Typography variant="h5" mb={"1em"}>Feed, play, and nurture your frog to keep it happy and healthy.</Typography>
+        <Typography variant="h3">Mint Commemorative NFTs</Typography><Typography variant="h5">Upon your frog&apos;s digital demise, mint commemorative NFTs to immortalize its memory.</Typography>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Typography variant='h5' mt={"2em"}>We&apos;re proud to support the Rainforest Foundation, a committed advocate for the rights and preservation efforts of Indigenous and traditional communities inhabiting the rainforests worldwide. Proceeds from NFT minting will go towards their vital conservation efforts. Learn more at <Link href={"https://rainforestfoundation.org"} target='__blank'>https://rainforestfoundation.org</Link>.</Typography>
+      </Card>
     </main>
   )
 }
